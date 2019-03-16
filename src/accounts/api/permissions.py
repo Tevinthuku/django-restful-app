6a8c223815558aea5303a6feaf6dbe433ed1dd24
Tevinthuku <tevinthuku@gmail.com>
2019-03-16 19:15:48 +0300

    Fix method

--- src/accounts/api/permissions.py
+++ src/accounts/api/permissions.py
@@ -4,11 +4,13 @@ from rest_framework import permissions
 class AnonPermissionOnly(permissions.BasePermission):
     message = "You are already authenticated, kindly logout and try again later"
 
-    def has_permission(self, request, view):
+    def has_permission(self, request, view, obj):
         return not request.user.is_authenticated()
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
 
-    def has_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
         return obj.owner == request.user
